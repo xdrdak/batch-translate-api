8692644d14dd9bda7fb78637a2db3bbf432d3e3d
Xavier Drdak <xdrdak@premiumbeat.com>
2018-02-23 18:15:08 -0500

    Added google-translate api and async calls

---
+++ languages.js
@@ -0,0 +1,23 @@
+const languages = [
+  'cs',
+  'da',
+  'de',
+  'es',
+  'fi',
+  'fr',
+  'hu',
+  'it',
+  'ja',
+  'ko',
+  'nb',
+  'nl',
+  'pl',
+  'pt',
+  'ru',
+  'sv',
+  'th',
+  'tr',
+  'zh',
+];
+
+module.exports = languages;
